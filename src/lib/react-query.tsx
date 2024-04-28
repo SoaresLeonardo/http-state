d1034c901d4442cf9973534b6672e8afd8bf9d06
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-04-28 17:57:01 -0300

    fix add suspense api

--- src/lib/react-query.tsx
+++ src/lib/react-query.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 
 export const queryClient = new QueryClient();
 
 export function Provider({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+    </QueryClientProvider>
   );
 }
